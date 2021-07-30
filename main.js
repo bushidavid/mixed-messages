6adc5d258cbe0db2839755357e829e362a2d497a
davidbushi <88148058+davidbushi@users.noreply.github.com>
2021-07-30 15:15:48 +0300

    final version

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 //generate random number
-
 const randomIndex = array => Math.floor(Math.random() * array.length)
 
 //array of prefixes for the message
 const prefixes = ['You are', 'I am'];
-const middles = ['the', 'incredibly', 'amazingly', 'formidably'];
+const middles = ['incredibly', 'amazingly', 'formidably'];
 const suffixes = ['talented', 'capable', 'skilled', 'educate', 'literate', 'passionate', 'kind', 'compassionate', 'rich'];
 
 
-const messageFactory = (prefixes, middles, suffixes) => {
-    return {
-        _prefix: prefixes,
-        _middle: middles,
-        _suffix: suffixes,
-        
+const messageGenerator = {
+    
+    _prefix: "",
+    _middle: "",
+    _suffix: "",
+    
+    set prefix(str){
+        this.prefix = str;
+    },
+
+    set middle(str){
+        this._middle = str;
+    },
+
+    set suffix(str){
+        this._suffix = str;
+    },
+
+    output(){
+        console.log(this._prefix + ' ' + this._middle + ' ' + this._suffix)
     }
+
 }
+
+
+messageGenerator._prefix = prefixes[randomIndex(prefixes)];
+messageGenerator._middle = middles[randomIndex(middles)];
+messageGenerator._suffix = suffixes[randomIndex(suffixes)];
+
+messageGenerator.output();
